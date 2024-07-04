fed3029be6e68a2c8132ef09e578ec2e8bade34e
syaifulk <khabibsyaiful0@gmail.com>
2024-07-04 12:55:50 +0700

    maaf pak terlambat

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterlist/firebase_options.dart';
+import 'package:flutterlist/useritem.dart';
+import 'package:flutterlist/userlist.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(UserList());
+}
